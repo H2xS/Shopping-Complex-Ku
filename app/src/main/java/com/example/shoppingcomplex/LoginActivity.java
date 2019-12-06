dac289c9100d2431b10a4a920952e7396ef8d453
AWx2 <shoppingcomplexku.info@!gmail.com>
2019-12-07 00:03:12 +0530

    First app

--- app/src/main/java/com/example/shoppingcomplex/LoginActivity.java
+++ app/src/main/java/com/example/shoppingcomplex/LoginActivity.java
@@ -10,22 +10,29 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.shoppingcomplex.Model.Users;
+import com.example.shoppingcomplex.Prevalent.Prevalent;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.rey.material.widget.CheckBox;
+
+import io.paperdb.Paper;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText InputPhoneNumber,InputPassword;
     private Button LoginButton;
     private ProgressDialog loadingBar;
+    private TextView AdminLink,NotAdminLink;
 
     private String parentDbName="Users";
+    private CheckBox chkBoxRememberMe;
 
 
     @Override
@@ -36,7 +43,13 @@ public class LoginActivity extends AppCompatActivity {
         LoginButton=(Button) findViewById(R.id.login_btn);
         InputPassword=(EditText) findViewById(R.id.login_password_input);
         InputPhoneNumber=(EditText) findViewById(R.id.login_phone_number_input);
+        AdminLink=(TextView) findViewById(R.id.admin_panel_link);
+        NotAdminLink=(TextView) findViewById(R.id.not_admin_panel_link);
+
         loadingBar = new ProgressDialog(this);
+
+        chkBoxRememberMe = (CheckBox) findViewById(R.id.remember_me_ckb);
+        Paper.init(this);
 
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +57,28 @@ public class LoginActivity extends AppCompatActivity {
             {
                 LoginUser();
 
+            }
+        });
+
+        AdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                LoginButton.setText("Login Admin");
+                AdminLink.setVisibility(View.INVISIBLE);
+                NotAdminLink.setVisibility(View.VISIBLE);
+                parentDbName="Admins";
+            }
+        });
+
+        NotAdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                LoginButton.setText("Login");
+                AdminLink.setVisibility(View.VISIBLE);
+                NotAdminLink.setVisibility(View.INVISIBLE);
+                parentDbName="Users";
             }
         });
     }
@@ -74,6 +109,13 @@ public class LoginActivity extends AppCompatActivity {
     }
     private  void AllowAccessToAccount(final String phone, final String password)
     {
+        if(chkBoxRememberMe.isChecked())
+        {
+            Paper.book().write(Prevalent.UserPhoneKey, phone);
+            Paper.book().write(Prevalent.UserPasswordKey, password);
+        }
+
+
         final DatabaseReference RootRef;
         RootRef = FirebaseDatabase.getInstance().getReference();
 
@@ -81,7 +123,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot)
             {
-                if (dataSnapshot.child("parentDbName").child(phone).exists())
+                if (dataSnapshot.child(parentDbName).child(phone).exists())
                 {
                     Users userData = dataSnapshot.child(parentDbName).child(phone).getValue(Users.class);
 
@@ -89,12 +131,29 @@ public class LoginActivity extends AppCompatActivity {
                     {
                         if (userData.getPassword().equals(password))
                         {
-                            Toast.makeText(LoginActivity.this, "Logged in Successfully...", Toast.LENGTH_SHORT).show();
+                            if (parentDbName.equals("Admins"))
+                            {
+                                Toast.makeText(LoginActivity.this, "Welcome Admin, You are logged in Successfully...", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+
+                                Intent intent=new Intent(LoginActivity.this, AdminCategoryActivity.class);
+                                startActivity(intent);
+                            }
+                            else if (parentDbName.equals("Users"))
+                            {
+                                Toast.makeText(LoginActivity.this, "Logged in Successfully...", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+
+                                Intent intent=new Intent(LoginActivity.this, HomeActivity.class);
+                                Prevalent.currentOnlineUser=userData;
+                                startActivity(intent);
+                            }
+
+                        }
+                        else
+                        {
                             loadingBar.dismiss();
-
-                            Intent intent=new Intent(LoginActivity.this, HomeActivity.class);
-                            startActivity(intent);
-
+                            Toast.makeText(LoginActivity.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
                         }
 
                     }
--- app/src/main/java/com/example/shoppingcomplex/MainActivity.java
+++ app/src/main/java/com/example/shoppingcomplex/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.shoppingcomplex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.shoppingcomplex.Model.Users;
+import com.example.shoppingcomplex.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button joinNowBtn,loginBtn;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         joinNowBtn = (Button) findViewById(R.id.main_join_now_btn);
         loginBtn = (Button) findViewById(R.id.main_login_btn);
+        loadingBar=new ProgressDialog(this);
+
+
+        Paper.init(this);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +51,74 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent=new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        String UserPhoneKey = Paper.book().read(Prevalent.UserPhoneKey);
+        String UserPasswordKey = Paper.book().read(Prevalent.UserPasswordKey);
+
+
+        if(UserPasswordKey !="" && UserPasswordKey !="" )
+        {
+            if (!TextUtils.isEmpty(UserPhoneKey) && !TextUtils.isEmpty(UserPasswordKey))
+            {
+                AllowAccess(UserPhoneKey, UserPasswordKey);
+
+                loadingBar.setTitle("Already Logged In");
+                loadingBar.setMessage("Please Wait.....");
+                loadingBar.setCanceledOnTouchOutside(false);
+
+                loadingBar.show();
+            }
+        }
+
+    }
+
+    private void AllowAccess(final String UserPhoneKey, final String UserPasswordKey)
+    {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.child("Users").child(UserPhoneKey).exists())
+                {
+                    Users userData = dataSnapshot.child("Users").child(UserPhoneKey).getValue(Users.class);
+
+                    if (userData.getPhone().equals(UserPhoneKey))
+                    {
+                        if (userData.getPassword().equals(UserPasswordKey))
+                        {
+                            Toast.makeText(MainActivity.this, "You are already logged in...", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+
+                            Intent intent=new Intent(MainActivity.this, HomeActivity.class);
+                            Prevalent.currentOnlineUser=userData;
+                            startActivity(intent);
+
+                        }
+                        else
+                        {
+                            loadingBar.dismiss();
+                            Toast.makeText(MainActivity.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Account with this "+UserPhoneKey+" number do not exists.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    //Toast.makeText(LoginActivity.this, "You need to create a new Account.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
